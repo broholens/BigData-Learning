4d6ebfac3d3ffac776f3723ad1a3e2e9ce3fc48a
broholens <broholens@outlook.com>
2021-05-28 23:33:53 +0800

    fix: submit job with --package

--- bigdata/spark_engine/kafka_age_avg.py
+++ bigdata/spark_engine/kafka_age_avg.py
@@ -4,6 +4,20 @@
 # https://stackoverflow.com/questions/60230124/pyspark-sql-utils-analysisexception-failed-to-find-data-source-kafka
 # https://alphaoragroup.com/2021/05/27/py4j-protocol-py4jjavaerror-an-error-occurred-while-calling-o65-load-java-lang-nosuchmethoderror/
 # https://stackoverflow.com/questions/54546513/py4jjavaerror-an-error-occurred-while-calling
+# https://stackoverflow.com/questions/67496998/pyspark-kafka-org-apache-spark-sql-kafka010-kafkasourceprovider-could-not-be
+# spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1 /opt/BigData-Learning/bigdata/spark_engine/kafka_age_avg.py
+# https://docs.databricks.com/delta/delta-streaming.html
+"""
+hadoop start cmd
+cd /home/z/Downloads/hadoop-3.3.0
+bin/hdfs namenode -format
+sbin/start-dfs.sh
+sbin/start-yarn.sh
+
+hadoop stop cmd
+sbin/stop-dfs.sh
+sbin/stop-yarn.sh
+"""
 
 from pyspark import SparkConf
 from pyspark.sql import SparkSession, functions
@@ -21,13 +35,12 @@ if __name__ == '__main__':
         .getOrCreate()
 
     df = spark \
-        .read \
+        .readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
         .option("subscribe", "student") \
-        .load()
-
-    df.selectExpr("CAST(value AS STRING)")
+        .load() \
+        .selectExpr("CAST(value AS STRING)")
 
     student_schema = StructType([
         StructField('age', IntegerType()),
@@ -35,11 +48,11 @@ if __name__ == '__main__':
         StructField('name', StringType())
     ])
 
-    ages = df.select(
+    df = df.select(
         functions.from_json(functions.col('value').cast('string'), student_schema).alias('parsed_value')
     ).select('parsed_value.age', 'parsed_value.gender', 'parsed_value.name')
 
-    query = ages\
+    query = df \
         .writeStream.outputMode("update").format("console") \
         .start()
 
