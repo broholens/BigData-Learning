84789ae5c0fb605dd1b144e78b08a8122aeff9b1
broholens <broholens@outlook.com>
2021-05-31 23:01:17 +0800

    feat: support run cmd on other nodes

--- bigdata/utils/conf_util.py
+++ bigdata/utils/conf_util.py
@@ -1,11 +1,29 @@
 import pathlib
 
 
-def load_hosts():
+def load_hosts_without_name():
+    conf_file = pathlib.Path(__file__).parent.parent / 'conf' / 'hosts.cfg'
+    with open(conf_file, 'r') as f:
+        return [i.strip().split(' ')[0] for i in f.readlines()]
+
+
+def load_hosts_with_name():
     conf_file = pathlib.Path(__file__).parent.parent / 'conf' / 'hosts.cfg'
     with open(conf_file, 'r') as f:
         return [i.strip() for i in f.readlines()]
 
 
+def load_hostname():
+    conf_file = pathlib.Path(__file__).parent.parent / 'conf' / 'hosts.cfg'
+    with open(conf_file, 'r') as f:
+        return [i.strip().split(' ')[-1] for i in f.readlines()]
+
+
+def load_other_hosts_without_name():
+    conf_file = pathlib.Path(__file__).parent.parent / 'conf' / 'hosts.cfg'
+    with open(conf_file, 'r') as f:
+        return [i.strip().split(' ')[-1] for i in f.readlines()][1:]
+
+
 if __name__ == '__main__':
-    print(load_hosts())
+    print(load_hosts_with_name())
--- bigdata/utils/ssh_util.py
+++ bigdata/utils/ssh_util.py
@@ -1,13 +1,14 @@
 from fabric import Connection, SerialGroup
-from bigdata.utils.conf_util import load_hosts
+from bigdata.utils.conf_util import load_hosts_without_name
 
 
 class SSHConnection:
     def __init__(self):
-        self.hosts = load_hosts()
+        self.hosts = load_hosts_without_name()
         self._localhost = 'localhost'
         self._local_connection = None
         self._connections = None
+        self._other_connections = None
 
     @property
     def connection(self):
@@ -20,9 +21,18 @@ class SSHConnection:
         if not self._connections:
             self._connections = SerialGroup(*self.hosts, user='root', connect_kwargs={'password': ' '})
         return self._connections
+    
+    @property
+    def other_connections(self):
+        if not self._other_connections:
+            self._connections = SerialGroup(*self.hosts[1:], user='root', connect_kwargs={'password': ' '})
+        return self._other_connections
 
     def run_cmd_local(self, cmd):
         return self.connection.run(cmd)
 
     def run_cmd_all_nodes(self, cmd):
         return self.connections.run(cmd)
+    
+    def run_cmd_other_nodes(self, cmd):
+        return self.other_connections.run(cmd)
